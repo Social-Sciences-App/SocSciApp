5dac2757813fa223c38de1b8fb0184e67303e7b0
Jason Wong <jasonlwong25@gmail.com>
2023-03-21 21:47:44 -0700

    Fixed scroll issue

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import {
   Dimensions,
   Image,
+  SafeAreaView,
   ScrollView,
   StyleSheet,
   Text,
   TouchableOpacity,
+  View,
 } from 'react-native';
 
 const styles = StyleSheet.create({
@@ -22,6 +24,7 @@ const styles = StyleSheet.create({
   flyer: {
     width: 300,
     height: 300,
+    alignSelf: 'center',
   },
   headerTextStyle: {
     fontWeight: 'bold',
@@ -37,32 +40,39 @@ const styles = StyleSheet.create({
     backgroundColor: '#F6EDA0',
     padding: 15,
     borderRadius: 15,
+    fontSize: 7,
+    margin: 40,
   },
 });
 
 const HomePage = (): JSX.Element => {
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Image
-        style={styles.banner}
-        source={require('../static/images/ssarc_banner.png')}
-      />
-      <Text style={styles.headerTextStyle}>About Us</Text>
-      <Text style={styles.textStyle}>
-        The Social Science Academic Resource Center (SSARC) was established over
-        three decades ago to assist students in the School of Social Sciences.
-        Schedule an appointment with us to gain assistance in your post-graduate
-        goals.
-      </Text>
-      <TouchableOpacity style={styles.button} onPress={() => {}}>
-        <Text>Click Here to Schedule an Appointment</Text>
-      </TouchableOpacity>
-      <Text style={styles.headerTextStyle}>RSVP to Upcoming SSARC Events</Text>
-      <Image
-        style={styles.flyer}
-        source={require('../static/images/SSARC_Summer_2021_Webinars.png')}
-      />
-    </ScrollView>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <Image
+          style={styles.banner}
+          source={require('../static/images/ssarc_banner.png')}
+        />
+        <Text style={styles.headerTextStyle}>About Us</Text>
+        <Text style={styles.textStyle}>
+          The Social Science Academic Resource Center (SSARC) was established
+          over three decades ago to assist students in the School of Social
+          Sciences. Schedule an appointment with us to gain assistance in your
+          post-graduate goals.
+        </Text>
+        <TouchableOpacity style={styles.button} onPress={() => {}}>
+          <Text>Click Here to Schedule an Appointment</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.headerTextStyle}>
+          RSVP to Upcoming SSARC Events
+        </Text>
+        <Image
+          style={styles.flyer}
+          source={require('../static/images/SSARC_Summer_2021_Webinars.png')}
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
